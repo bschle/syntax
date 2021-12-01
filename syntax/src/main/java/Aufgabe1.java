46caa41177ad6f79b6411ac29b31e4d681b0d722
bschle <schaadlevin@outlook.de>
2021-12-01 15:00:46 +0100

    i updated

---
+++ syntax/src/main/java/Aufgabe1.java
@@ -0,0 +1,16 @@
+public class Aufgabe1 {
+    public static void main(String[] args) {
+
+        int a = -8;
+		double b = 10.0;
+		//Dies ist ein Kommentar
+        double resultat = a + b;
+                String text = "Das Resultat ist:" + resultat;
+
+        if (resultat < 10.0) {
+            System.out.println("Gratulation!");
+        }	 else{
+                System.out.println(text);
+                }
+    }
+}
