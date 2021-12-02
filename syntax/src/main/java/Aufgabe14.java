4013d2cdab5dad8d9369a30b2b19749293827900
bschle <schaadlevin@outlook.de>
2021-12-02 15:57:08 +0100

    i updated

--- syntax/src/main/java/Aufgabe14.java
+++ syntax/src/main/java/Aufgabe14.java
@@ -1,21 +1,26 @@
+import java.util.Arrays;
+
 public class Aufgabe14 {
     public static void main(String[] args) {
 
         String[] nadeln = new String[11];
-        for (int i = 0; i < nadeln.length; i++){
-            nadeln[i] = " ";
-        }
-        nadeln[nadeln.length / 2] = "*";
-        int positionl = nadeln.length / 2 - 1;
-        int positionr = nadeln.length / 2 + 1;
-        for(int i = 0; i < nadeln.length / 2; i++){
-            for (String number : nadeln){
-                System.out.print(number);}
-            nadeln[positionl] = "*";
-            nadeln[positionr] = "*";
-            positionl -= 1;
-            positionr += 1;
+        Arrays.fill(nadeln, " ");
+        String star = "*";
+        int x = 0;
+
+        for (int i = 0; i < 11 / 2; i++) {
+
+            nadeln[nadeln.length / 2 - x] = star;
+            for (String number : nadeln) {
+                System.out.print(number);
+            }
+
+            x += 1;
+            star += "**";
             System.out.print("\n");
+
+            Arrays.fill(nadeln, " ");
+
         }
     }
 }
